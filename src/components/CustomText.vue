174773d6474c38da838ceda498fd2f1e47b5d6a8
Selim <ahmetselimpolat1625@gmail.com>
2020-09-07 20:51:01 +0300

    First commit
    Created all files

---
+++ src/components/CustomText.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="tag" :class="size">
+    <slot />
+  </component>
+
+</template>
+
+<script>
+export default {
+  name: 'CustomText',
+  props: {
+    tag: {
+      type: String,
+      default: "span"
+    },
+    size: {
+      type: String,
+      default: "font-size-3", // normal:14, small:12, xsmall:11, xxsmall:10
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <CustomText>Twitch cloneee</CustomText>
+    <IconHome />
+  </div>
+</template>
+
+<script>
+
+import CustomText from '@/components/CustomText'
+import IconHome from '../../icons/home.svg'
+
+export default {
+  name: 'Home',
+  components: {
+    CustomText,
+    IconHome
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+
+  @media (--t) {
+    color: blue;
+    --font-size-3:
+  }
+}
+</style>
