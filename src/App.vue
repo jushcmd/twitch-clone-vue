04a5fc882caafb4fe385f675a9884dfb4c1bb9f9
Selim <ahmetselimpolat1625@gmail.com>
2020-09-12 03:51:53 +0300

    vuetify kuruldu ve card eklendi

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <!-- Header Bölümü -->
+
+    <!-- Header Bölümü END -->
+
+
+      <!-- Container -->
+      <v-content>
+        <v-container>
+          <v-row class="mb-4">
+            <v-col sm="6" md="4" lg="3" xl="2" v-for="(item,index) in 3" :key="index">
+            <v-card>
+              <v-img height="248" src="https://raw.githubusercontent.com/SelimPolat16/twitch-clone-vue/vue_eskiversion/src/images/kodbilbakkal.png"
+              lazy-src="https://raw.githubusercontent.com/SelimPolat16/twitch-clone-vue/vue_eskiversion/src/images/kodbilbakkal.png" />
+           
+              <v-card-subtitle class=""></v-card-subtitle>
+                 <v-avatar>
+      <img
+        src="https://static-cdn.jtvnw.net/jtv_user_pictures/1872225b-c797-4b35-b172-007c1a1fdb66-profile_image-70x70.png"
+        alt="Kodbilen"
+      >
+    </v-avatar>
+              <v-card-text class="text--primary"><div>Kodbilen Yayında | !çöp !düş</div><div>Kodbilen</div></v-card-text>
+              <v-card-actions>
+                <v-btn color="purple" text>a</v-btn>
+              </v-card-actions>
+            </v-card>
+            </v-col>
+            </v-row>
+        </v-container>
+      </v-content>
+    <!-- Container END -->
+  </v-app>
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: "App",
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  })
+};
+</script>
